8efee52559417a0638e8f85f09d822bc6eafcf9a
Vera <Vera@LAPTOP-2PS2I9DA>
2021-12-20 22:24:36 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение");
+            string startString = Console.ReadLine();
+            string[] stringArray = startString.Split();
+            int rezultString = 0;
+            foreach (string s in stringArray)
+                if (s.Length > rezultString)
+                {
+                    rezultString = s.Length;
+
+                }
+            Console.WriteLine(rezultString);
+            Console.ReadKey();
+        }
+    }
+
+}
+
---
+++ z2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace z2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("Введите предложение");
+                string startString = Console.ReadLine();
+                startString = startString.Replace(" ", "");
+                char[] massivSlov = startString.ToCharArray();
+                Array.Reverse(massivSlov);
+                string lastString = new string(massivSlov);
+                if (startString == lastString)
+                {
+                    Console.WriteLine("Строка является палиндромом");
+                }
+                else
+                { 
+                    Console.WriteLine("Не является палиндромом"); 
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
